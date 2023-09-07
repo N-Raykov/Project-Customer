67502560e6ef5cd7d549548b48f2a43a793471a0
N-Raykov <nikolaradoslavovraykov@gmail.com>
2023-09-07 15:27:23 +0200

    Improved the enemy AI

--- Assets/Scripts/Enemy/EnemyMove.cs
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -7,23 +7,110 @@ public class EnemyMove : MonoBehaviourWithPause
 {
 
     [SerializeField] GameObject player;
+    [SerializeField] float minStrafeDuration;
+    [SerializeField] float maxStrafeDuration;
+
+    [Header("Ranges")]
+    [SerializeField] float aggroRange;
+    [SerializeField] float preferredRange;
+    [SerializeField] float minRange;
+
+    [Header("Speeds")]
+    [SerializeField] float speed;
+    [SerializeField] float strafeSpeed;
+    [SerializeField] float moonWalkSpeed;
+    [SerializeField] float rotationSpeed;
     NavMeshAgent agent;
+
+    private enum EnemyState
+    {
+        Aggro,
+        PreferredRange,
+        MinRange,
+        Stunned
+    }
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
     }
 
+    private EnemyState currentState = EnemyState.Aggro;
+    private float movementTimer;
+
     protected override void UpdateWithPause()
     {
-        Vector3 toPlayer = player.transform.position - transform.position;
-        if (Vector3.Distance(player.transform.position, transform.position) < 3)
+        switch (currentState)
         {
-            Vector3 targetPosition = toPlayer.normalized * -3f;
-            agent.destination = targetPosition;
-        }
+            case EnemyState.Aggro:
+                if (Vector3.Distance(player.transform.position, transform.position) < aggroRange && Vector3.Distance(player.transform.position, transform.position) > preferredRange)
+                {
+                    agent.SetDestination(player.transform.position);
+                    agent.speed = speed;
+                }
+                else
+                {
+                    currentState = EnemyState.PreferredRange;
+                    movementTimer = Time.time;
+                }
+                break;
 
-        agent.destination = player.transform.position;
-        transform.LookAt(player.transform);
+            case EnemyState.PreferredRange:
+                if (Vector3.Distance(player.transform.position, transform.position) < preferredRange && Vector3.Distance(player.transform.position, transform.position) > minRange)
+                {
+                    agent.speed = strafeSpeed;
+                    if (Time.time >= movementTimer)
+                    {
+                        RandomlySwitchDirection();
+                        ResetMovementTimer();
+                    }
+                }
+                else
+                {
+                    currentState = EnemyState.MinRange;
+                }
+                break;
+
+            case EnemyState.MinRange:
+                if (Vector3.Distance(player.transform.position, transform.position) < minRange)
+                {
+                    agent.speed = moonWalkSpeed;
+                    Vector3 toPlayer = player.transform.position - transform.position;
+                    Vector3 targetPosition = transform.position - toPlayer.normalized * 5f;
+                    agent.SetDestination(targetPosition);
+                }
+                else
+                {
+                    currentState = EnemyState.Aggro;
+                }
+                break;
+
+            case EnemyState.Stunned:
+                //do nothing
+                break;
+        }
+        Vector3 direction = player.transform.position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    private void RandomlySwitchDirection()
+    {
+        float randomValue = Random.value;
+        if (randomValue < 0.5f)
+        {
+            // Move left
+            agent.destination = transform.position - transform.right * 5f;
+        }
+        else
+        {
+            // Move right
+            agent.destination = transform.position + transform.right * 5f;
+        }
+    }
+
+    private void ResetMovementTimer()
+    {
+        movementTimer = Time.time + Random.Range(minStrafeDuration, maxStrafeDuration);
     }
 }
